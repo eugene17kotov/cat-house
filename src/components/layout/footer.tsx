53e3352eb951d60f5c0cbf652ab595a583e85199
Yevnehii <eu.kobets@gmail.com>
2025-09-03 04:44:40 +0300

    feat: change footer height

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Footer() {
   return (
     <footer className='border-t bg-background'>
-      <div className='container mx-auto px-4 py-8 md:py-12'>
+      <div className='container mx-auto px-4 py-4 md:py-6'>
         <div className='flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0'>
           <div className='flex flex-col items-center md:items-start space-y-4 md:space-y-2'>
             <Link href='/' className='flex items-center space-x-2'>
